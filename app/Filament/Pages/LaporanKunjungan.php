5c37238eb82951069c167b88e8ef2e4ae5f3efa1
Dio Prasetyo <prasetyodio04@gmail.com>
2026-07-02 02:31:51 +0700

    Update Final Source Code

--- app/Filament/Pages/LaporanKunjungan.php
+++ app/Filament/Pages/LaporanKunjungan.php
@@ -95,7 +95,8 @@ class LaporanKunjungan extends Page implements HasForms
                         ->default('1')
                         ->required()
                         ->live()
-                        ->visible(fn(\Filament\Schemas\Components\Utilities\Get $get) => $get('mode') === 'daily_week'),
+                        ->visible(fn(\Filament\Schemas\Components\Utilities\Get $get) => $get('mode') === 'daily_week')
+                        ->columnSpanFull(),
                 ]),
 
                 Grid::make(2)->schema([
--- app/Filament/Pages/LaporanPengembalian.php
+++ app/Filament/Pages/LaporanPengembalian.php
@@ -96,7 +96,8 @@ class LaporanPengembalian extends Page implements HasForms
                         ->default('1')
                         ->required()
                         ->live()
-                        ->visible(fn(\Filament\Schemas\Components\Utilities\Get $get) => $get('mode') === 'daily_week'),
+                        ->visible(fn(\Filament\Schemas\Components\Utilities\Get $get) => $get('mode') === 'daily_week')
+                        ->columnSpanFull(),
                 ]),
 
                 Grid::make(2)->schema([
